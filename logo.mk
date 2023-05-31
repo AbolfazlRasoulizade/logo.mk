d36e4e759bda0ed22139be98a00e5a99f7d0f0f0
Abolfazl rasoulizade <77486257+abolfazlrasoulizade@users.noreply.github.com>
2023-05-31 18:23:47 +0330

    Create logo.mk

---
+++ logo.mk
@@ -0,0 +1,28 @@
+#Alwatr Logo 
+
+```
+                                                                                                                                                      
+                                                                                                                                                      
+               AAA               LLLLLLLLLLL     WWWWWWWW                           WWWWWWWW   AAA         TTTTTTTTTTTTTTTTTTTTTTTRRRRRRRRRRRRRRRRR   
+              A:::A              L:::::::::L     W::::::W                           W::::::W  A:::A        T:::::::::::::::::::::TR::::::::::::::::R  
+             A:::::A             L:::::::::L     W::::::W                           W::::::W A:::::A       T:::::::::::::::::::::TR::::::RRRRRR:::::R 
+            A:::::::A            LL:::::::LL     W::::::W                           W::::::WA:::::::A      T:::::TT:::::::TT:::::TRR:::::R     R:::::R
+           A:::::::::A             L:::::L        W:::::W           WWWWW           W:::::WA:::::::::A     TTTTTT  T:::::T  TTTTTT  R::::R     R:::::R
+          A:::::A:::::A            L:::::L         W:::::W         W:::::W         W:::::WA:::::A:::::A            T:::::T          R::::R     R:::::R
+         A:::::A A:::::A           L:::::L          W:::::W       W:::::::W       W:::::WA:::::A A:::::A           T:::::T          R::::RRRRRR:::::R 
+        A:::::A   A:::::A          L:::::L           W:::::W     W:::::::::W     W:::::WA:::::A   A:::::A          T:::::T          R:::::::::::::RR  
+       A:::::A     A:::::A         L:::::L            W:::::W   W:::::W:::::W   W:::::WA:::::A     A:::::A         T:::::T          R::::RRRRRR:::::R 
+      A:::::AAAAAAAAA:::::A        L:::::L             W:::::W W:::::W W:::::W W:::::WA:::::AAAAAAAAA:::::A        T:::::T          R::::R     R:::::R
+     A:::::::::::::::::::::A       L:::::L              W:::::W:::::W   W:::::W:::::WA:::::::::::::::::::::A       T:::::T          R::::R     R:::::R
+    A:::::AAAAAAAAAAAAA:::::A      L:::::L         LLLLLLW:::::::::W     W:::::::::WA:::::AAAAAAAAAAAAA:::::A      T:::::T          R::::R     R:::::R
+   A:::::A             A:::::A   LL:::::::LLLLLLLLL:::::L W:::::::W       W:::::::WA:::::A             A:::::A   TT:::::::TT      RR:::::R     R:::::R
+  A:::::A               A:::::A  L::::::::::::::::::::::L  W:::::W         W:::::WA:::::A               A:::::A  T:::::::::T      R::::::R     R:::::R
+ A:::::A                 A:::::A L::::::::::::::::::::::L   W:::W           W:::WA:::::A                 A:::::A T:::::::::T      R::::::R     R:::::R
+AAAAAAA                   AAAAAAALLLLLLLLLLLLLLLLLLLLLLLL    WWW             WWWAAAAAAA                   AAAAAAATTTTTTTTTTT      RRRRRRRR     RRRRRRR
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
